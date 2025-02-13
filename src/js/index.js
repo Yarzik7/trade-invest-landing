2ec8cc555a9cd7c0cfb90f311edf4bab9b60de2f
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2025-02-13 23:26:42 +0200

    feat(reviews): add reviews section

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,2 @@
 import './burgerMenu.js';
+import './reviewsSlider.js';
---
+++ src/js/reviewsSlider.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.reviews-swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.reviews-slider-button-next',
+    prevEl: '.reviews-slider-button-prev',
+  },
+  slidesPerView: 1,
+  spaceBetween: 16,
+  loop: true,
+});
